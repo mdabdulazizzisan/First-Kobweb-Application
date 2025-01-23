9d6559b9bb988bae2f279924171bd1f237de59fd
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-23 12:15:50 +0600

    create hello world with font awesome icon

--- site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
+++ site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
@@ -1,18 +1,28 @@
 package com.kolu.learningkobweb.pages
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.*
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSurprise
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row {
+            FaFaceSurprise()
+            Text(
+                value = "Hello World"
+            )
+        }
     }
 }
