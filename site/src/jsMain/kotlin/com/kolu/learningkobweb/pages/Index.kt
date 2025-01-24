7bebdbe93a52f6921a9788aeb9022c177a280aab
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-24 09:29:45 +0600

    rewrite the initial Hello World page.

--- site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
+++ site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
@@ -5,8 +5,13 @@ import com.varabyte.kobweb.compose.foundation.layout.*
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSurprise
+import com.varabyte.kobweb.silk.components.icons.fa.*
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
 @Page
@@ -18,11 +23,23 @@ fun HomePage() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Row {
-            FaFaceSurprise()
-            Text(
-                value = "Hello World"
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            FaFaceSmile(
+                modifier = Modifier.margin(right = 10.px),
+                size = IconSize.XXL,
+                style = IconStyle.OUTLINE
             )
+            P(
+                attrs = Modifier
+                    .fontSize(40.px)
+                    .toAttrs()
+            ) {
+                Text(
+                    value = "Hello World"
+                )
+            }
         }
     }
 }
