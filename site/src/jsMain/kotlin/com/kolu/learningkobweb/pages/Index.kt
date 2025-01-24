3fc1194a47078280cb54da6469208a78db908a22
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-24 10:13:56 +0600

    create an input box that updates the name.

--- site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
+++ site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
@@ -7,16 +7,21 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
 import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.icons.fa.*
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.placeholder
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
+    var name by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -37,9 +42,20 @@ fun HomePage() {
                     .toAttrs()
             ) {
                 Text(
-                    value = "Hello World"
+                    value = "Hello $name"
                 )
             }
+
         }
+        Input(
+            type = InputType.Text,
+            attrs = Modifier
+                .fontSize(20.px)
+                .padding(leftRight = 20.px, topBottom = 10.px)
+                .toAttrs(){
+                    placeholder(value = "Enter Your Name: ")
+                    onInput { name = it.value }
+                }
+        )
     }
 }
