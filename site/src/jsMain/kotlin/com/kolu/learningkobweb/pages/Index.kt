5c9ae659009412e4d70f5d4d5eac8d5346dc8afc
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-26 19:02:31 +0600

    add minor updated.

--- site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
+++ site/src/jsMain/kotlin/com/kolu/learningkobweb/pages/Index.kt
@@ -5,12 +5,14 @@ import com.varabyte.kobweb.compose.foundation.layout.*
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.icons.fa.*
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Text
 import com.varabyte.kobweb.silk.components.forms.Button
+import org.jetbrains.compose.web.dom.P
 import kotlin.random.Random
 
 @Page
@@ -47,7 +49,8 @@ fun HomePage() {
                         .margin(leftRight = 10.px),
                     onClick = {
                         playerSelected = 0
-                        winner = checkWin(playerSelected)
+                        computerSelected = getComputerSelected()
+                        winner = playerSelected == computerSelected
                     },
                 ){
                     Text("Rock")
@@ -56,8 +59,10 @@ fun HomePage() {
                     modifier = Modifier
                         .size(100.px)
                         .margin(leftRight = 10.px),
-                    onClick = { playerSelected = 1
-                        winner = checkWin(playerSelected) },
+                    onClick = {
+                        playerSelected = 1
+                        computerSelected = getComputerSelected()
+                        winner = playerSelected == computerSelected },
                 ){
                     Text("Paper")
                 }
@@ -67,21 +72,43 @@ fun HomePage() {
                         .margin(leftRight = 10.px),
                     onClick = {
                         playerSelected = 2
-                        winner = checkWin(playerSelected) },
+                        computerSelected = getComputerSelected()
+                        winner = playerSelected == computerSelected },
                 ){
                     Text("Scissor")
                 }
             }
-            Text(
-                value = if (winner) "You selected $playerSelected Winner!" else "you selected $playerSelected Looser!"
-            )
+
+            P(
+                attrs = Modifier
+                    .fontSize(40.px)
+                    .margin(top = 20.px)
+                    .toAttrs()
+            ) {
+                Text(
+                    value = if (winner) "You selected ${selectedToString(playerSelected)} \n" +
+                            "and I selected ${selectedToString(computerSelected)} \n" +
+                            "You Win!!!" else "You selected ${selectedToString(playerSelected)} \n" +
+                            "and I selected ${selectedToString(computerSelected)} \n" +
+                            "You Loose!!!",
+                )
+            }
         }
 
 
     }
 }
 
-fun checkWin(selected: Int): Boolean{
-    val winner = Random.nextInt(0,3)
-    return winner == selected
+
+fun getComputerSelected(): Int{
+    return Random.nextInt(0,3)
+}
+
+fun selectedToString(selected: Int): String{
+    return when(selected){
+        0 -> "rock"
+        1 -> "Paper"
+        2 -> "Scissor"
+        else -> "Invalid"
+    }
 }
